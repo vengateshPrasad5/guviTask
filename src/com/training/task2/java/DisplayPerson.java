14f2e0a41f3698315b5d8a2a776955cede77dd7c
Dasrap <vengatesh@automaton.com>
2024-01-29 06:40:31 +0530

    solution T2 1.1

---
+++ src/com/training/task2/java/DisplayPerson.java
@@ -0,0 +1,21 @@
+package com.training.task2.java;
+
+import java.sql.SQLOutput;
+
+public class DisplayPerson extends Person {
+    public static void main (String[] args){
+        Person person  = new Person();
+// get default name and age
+        System.out.println("Before initialize "+person.getName());// Output : Prasad
+        System.out.println("Before initialize " + person.getAge());// Output : 18
+
+// set name and age
+        person.setName("Vengatesh");
+        person.setAge(23);
+
+// get name and age after initialized
+        System.out.println("After initialize "+person.getName());// Output : Vengatesh
+        System.out.println("After initialize " + person.getAge());// Output : 23
+
+    }
+}
---
+++ src/com/training/task2/java/Person.java
@@ -0,0 +1,26 @@
+package com.training.task2.java;
+
+public class Person {
+
+//    initialize name and age
+    public String name = "Prasad";
+    public int age =18;
+
+//    getter
+    public String getName() {
+        return name;
+    }
+    public int getAge() {
+        return age;
+    }
+
+
+
+//    setter
+    public void setName(String name) {
+        this.name = name;
+    }
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
