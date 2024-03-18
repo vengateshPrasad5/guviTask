a7012cd8f55154b145fb3b3f1ae42c9734ed5bf8
Dasrap <vengatesh@automaton.com>
2024-03-18 16:29:14 +0530

    jdbc practice

--- src/com/training/JDBCdemo.java
+++ src/com/training/JDBCdemo.java
@@ -3,16 +3,33 @@ package com.training;
 import java.sql.*;
 
 public class JDBCdemo {
+        static String url = "jdbc:mysql://localhost/empdetails";
+        static String userName = "root";
+        static String password = "Vengat5796!";
+
     public static void main(String[] args) throws SQLException {
-        Connection connection = DriverManager.getConnection("jdbc:mysql://localhost/employeedb?user=root&password=Vengat5796!");
 
-        PreparedStatement preparedStatement = connection.prepareStatement(" SELECT * FROM employee");
+        updateRecord();
 
-        ResultSet resultSet = preparedStatement.executeQuery();
+    }
 
-        while (resultSet.next()){
-         System.out.println(resultSet.getInt(1)+" "+resultSet.getString(2));
-        }
+    private static void updateRecord() throws SQLException {
+        Connection connection = DriverManager.getConnection(url,userName,password);
+
+        int empCode = 123;
+        String empname = "ravi";
+        int empAge = 25;
+        int esalary = 8000;
+
+        String query  = "insert into employee values(?,?,?,?)";
+        PreparedStatement preparedStatement = connection.prepareStatement(query);
+        preparedStatement.setInt(1,empCode);
+        preparedStatement.setString(2,empname);
+        preparedStatement.setInt(3,empAge);
+        preparedStatement.setInt(4,esalary);
+
+        int rows = preparedStatement.executeUpdate();
+        System.out.println("no of rows inserted " + rows);
     }
 
 }
